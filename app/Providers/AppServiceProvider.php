596442bba7764301db998773ee78a4ff5bd3c3fe
angkomuhtar <angkomuhtar@gmail.com>
2023-10-09 04:47:13 +0000

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (config('app.env') == 'production') {
+            # code...
+            \URL::forceScheme('https');
+        }
     }
 }
